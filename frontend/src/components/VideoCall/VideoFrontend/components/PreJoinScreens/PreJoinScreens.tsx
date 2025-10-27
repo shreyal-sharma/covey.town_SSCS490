b4b556e3f12c9d39e6ea72c094d2bfa2270d8470
abigail <abigail@dogbuilt.net>
2025-10-27 17:08:32 -0400

    Add hello world for deployment demo

--- frontend/src/components/VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens.tsx
+++ frontend/src/components/VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens.tsx
@@ -37,6 +37,7 @@ export default function PreJoinScreens() {
     <IntroContainer>
       <MediaErrorSnackbar error={mediaError} />
       <Heading as="h2" size="xl">Welcome to Covey.Town!</Heading>
+      <Heading as="h3" size="xl">Hello, world!</Heading>
       <Text p="4">
         Covey.Town is a social platform that integrates a 2D game-like metaphor with video chat.
         To get started, setup your camera and microphone, choose a username, and then create a new town
